a6074739269bfb3f7424abc0f527e8c29d652382
nagzpy <nagpadmahas@gmail.com>
2025-09-16 16:13:40 -0500

    Added labels

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MyFirstMAUIMobileApp
+﻿using MyFirstMAUIMobileApp.Views;
+
+namespace MyFirstMAUIMobileApp
 {
     public partial class App : Application
     {
@@ -6,7 +8,8 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            //MainPage = new AppShell();
+            MainPage = new MainPage();
         }
     }
 }
