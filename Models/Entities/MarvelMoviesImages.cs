b6894534656927605c0a2275a6cd6554b19adbf7
fishnu223 <nagpadmahas@gmail.com>
2025-12-01 16:19:27 -0600

    Made Changes

---
+++ Models/Entities/MarvelMoviesImages.cs
@@ -0,0 +1,25 @@
+﻿//Ensure your NAMESPACE is correct
+namespace MyFirstMAUIMobileApp.Models.Entities;
+
+public class MarvelMoviesImages
+{
+    public string NameOfActor { get; set; }
+    public string NameInMovie { get; set; }
+
+    public MarvelMoviesImages()
+    {
+
+    }
+
+    public MarvelMoviesImages(string name, string moviename)
+    {
+        NameOfActor = name; 
+        NameInMovie = moviename;
+    }
+    public static List<MarvelMoviesImages> GetActors() => new List<MarvelMoviesImages>
+    {
+        new MarvelMoviesImages("Chris Pratt"),
+        new MarvelMoviesImages("Zoe Saldana"),
+        new MarvelMoviesImages("Chris Pratt"),
+     };
+}
